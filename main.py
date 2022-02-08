41f1a3f9a6a65d85eddf279c04a103df24b06285
OmarVillalobos <omar_vr@live.com.mx>
2022-02-07 19:07:06 -0600

    changes
    dt.Tickers

--- main.py
+++ main.py
@@ -14,12 +14,8 @@ import visualizations as vs
 #%% Pasive Investment
 results_pasive = fun.pasive(dt.data_M,dt.weights_i.iloc[:,1].to_numpy(), dt.capital*dt.weights_initial.iloc[34,1], dt.capital)
 results_pasive = fun.tabla_rend(results_pasive)
-
+print(results_pasive)
 
 #%% Active Investment
 efficient_w = fun.port_emv(dt.Returns_d,dt.rf,dt.Sigma_d,4)
-weights_sharpe = fun.short_weigths(dt.Tickers[Tickers != "MXN.MX"].to_list(),efficient_w)
-
-#%%
-vs.pie_com(example)
-vs.returns_plot(results_pasive)
+weights_sharpe = fun.short_weigths(dt.Tickers[dt.Tickers != "MXN.MX"].to_list(),efficient_w)
